0e48ee3c3b0cc5dbbf0ad3add910ce7129656f54
CaioBoh <caioboh@hotmail.com>
2025-06-24 20:00:59 -0300

    Create templates

--- app/views.py
+++ app/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Product
 
 # Create your views here.
 def home(request):
     return render(request, 'app/templates/home.html')
 
 def products(request):
-    return render(request, 'app/templates/products.html')
+    products = Product.objects.all()
+    return render(request, 'app/templates/products.html', {'products': products})
 
-def categories(request):
-    return render(request, 'app/templates/categories.html')
+def produtDetail(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    return render(request, 'app/templates/productDetail.html', {
+        'product' : product,
+    })
